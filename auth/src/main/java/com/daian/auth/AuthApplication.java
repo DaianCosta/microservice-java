37f548c81f3392c1c31c523e12f7f448d6730bb3
Daian Costa <daiancosta@Daians-MacBook-Pro.local>
2021-07-10 19:55:34 -0300

    add

--- auth/src/main/java/com/daian/auth/AuthApplication.java
+++ auth/src/main/java/com/daian/auth/AuthApplication.java
@@ -7,14 +7,14 @@ import com.daian.auth.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Arrays;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class AuthApplication {
 
 	public static void main(String[] args) {
